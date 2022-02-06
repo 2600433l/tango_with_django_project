514b86a7d238992e9c10569723a265ce096323f0
Julian Lee <2600433l@student.gla.ac.uk>
2022-02-06 21:12:20 +0000

    Revert "first commit"
    This reverts commit 160e03476ae4fc5675ce5cbe3341fe8a9e4e52fc.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,6 +23,5 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
