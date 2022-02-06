0b1e12878739a0277a8c83cfbf9db211d1dbd65a
Julian Lee <2600433l@student.gla.ac.uk>
2022-02-06 22:49:58 +0000

    chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
@@ -25,4 +27,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
